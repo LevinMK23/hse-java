98bb82b9638bcce7c6c1ebd215de6fc220dc70a9
TJirden <badert.06.amir@gmail.com>
2026-01-31 14:11:09 +0300

    Пробный коммит и пуш 2

--- src/main/java/hse/java/practice/task1/RubiksCube.java
+++ src/main/java/hse/java/practice/task1/RubiksCube.java
@@ -12,7 +12,7 @@ public class RubiksCube implements Cube {
      * грани разместить по ордеру в енуме цветов
      * грань 0 -> цвет 0
      * грань 1 -> цвет 1
-     * и тд
+     * и тд 2
      */
     public RubiksCube() {
         state = new int[49];
