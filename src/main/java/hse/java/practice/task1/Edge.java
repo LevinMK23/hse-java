9ed04ddb79071a08818503edff22b68a804d0945
Mac <doremiheev@vk.com>
2026-01-31 22:18:37 +0300

    Testing test 1

--- src/main/java/hse/java/practice/task1/Edge.java
+++ src/main/java/hse/java/practice/task1/Edge.java
@@ -35,4 +35,62 @@ public class Edge {
     public String toString() {
         return Arrays.deepToString(parts);
     }
+    //TODO:
+    public void rotateFace(RotateDirection direction){
+
+        switch (direction){
+            case CLOCKWISE:
+                rotateFaceClockwise();
+                break;
+            case COUNTERCLOCKWISE:
+                rotateFaceCounterClockwise();
+                break;
+        }
+
+    }
+
+    private void rotateFaceClockwise(){
+        CubeColor[][] transpotedColors = new CubeColor[3][3];
+
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
+            for (int colIndex = 0; colIndex < 3; colIndex++) {
+                transpotedColors[rowIndex][colIndex] = parts[colIndex][rowIndex];
+            }
+        }
+
+        parts = transpotedColors.clone();
+    }
+
+    private void rotateFaceCounterClockwise(){
+        for (int i = 0; i < 3; i++) {
+            rotateFaceClockwise();
+        }
+    }
+
+    public CubeColor[] getRow(int index) {
+        return parts[index];
+    }
+
+
+    public void setRow(int index, CubeColor[] colors){
+        parts[index] = colors;
+    }
+
+    public CubeColor[] getCol(int index){
+        CubeColor[] tmp = new CubeColor[3];
+
+        for (int rowIndex : new int[]{0, 1, 2}){
+            tmp[rowIndex] = parts[rowIndex][index];
+        }
+
+        return tmp;
+    }
+
+    public void setCol(int index, CubeColor[] colors){
+        for (int rowIndex : new int[]{0,1,2}) {
+            parts[rowIndex][index] = colors[rowIndex];
+        }
+    }
+
 }
+
--- src/main/java/hse/java/practice/task1/RubiksCube.java
+++ src/main/java/hse/java/practice/task1/RubiksCube.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Необходимо реализовать интерфейс Cube
  * При повороте передней грани, меняются верх низ право и лево
  */
-public class RubiksCube {
+public class RubiksCube implements Cube{
 
     private static final int EDGES_COUNT = 6;
 
@@ -26,12 +26,82 @@ public class RubiksCube {
         }
     }
 
-    public void front(RotateDirection direction) {
+    @Override
+    public void up(RotateDirection direction) {
+        edges[0].rotateFace(direction);
+    }
+
+    @Override
+    public void down(RotateDirection direction) {
 
     }
-    
+
+    @Override
+    public void left(RotateDirection direction) {
+
+    }
+
+    @Override
+    public void right(RotateDirection direction) {
+
+    }
+
+    //TODO:
+    @Override
+    public void front(RotateDirection direction) {
+
+        switch (direction){
+            case CLOCKWISE:
+                rotateFrontClockwise();
+                break;
+            case COUNTERCLOCKWISE:
+                rotateFrontCounterClockwise();
+                break;
+        }
+    }
+
+    @Override
+    public void back(RotateDirection direction) {
+
+    }
+
     public Edge[] getEdges() {
         return edges;
+    }
+    //TODO:
+    public void rotateFrontClockwise(){
+        edges[4].rotateFace(RotateDirection.CLOCKWISE);
+
+        CubeColor[] upToRight = edges[0].getRow(2);
+
+        CubeColor[] leftToUp = edges[2].getCol(2);
+        leftToUp = reverseArr(leftToUp);
+        edges[0].setRow(2, leftToUp);
+
+        CubeColor[] downToLeft = edges[1].getRow(0);
+        edges[2].setCol(2,downToLeft);
+
+        CubeColor[] rightToDown = edges[3].getCol(0);
+        rightToDown = reverseArr(rightToDown);
+        edges[1].setRow(0,rightToDown);
+
+        edges[3].setCol(0, upToRight);
+
+    }
+
+    public void rotateFrontCounterClockwise(){
+        rotateFrontClockwise();
+        rotateFrontClockwise();
+        rotateFrontClockwise();
+    }
+
+    private CubeColor[] reverseArr(CubeColor[] arr){
+        CubeColor[] tmp = arr.clone();
+        int len = arr.length;
+        for (int i = 0; i < len; i++){
+            tmp[i] = arr[len - 1 - i];
+        }
+        return tmp;
     }
 
     @Override
