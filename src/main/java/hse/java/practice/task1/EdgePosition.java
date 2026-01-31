77a5cd46f15f46130043cf65bcbf05cea935c3df
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-01-31 22:24:39 +0300

    first-commit : testing code

--- src/main/java/hse/java/practice/task1/EdgePosition.java
+++ src/main/java/hse/java/practice/task1/EdgePosition.java
@@ -8,11 +8,24 @@ package hse.java.practice.task1;
  * edges[2] -> LEFT
  * ...
  */
+
+// то есть имеется ввиду , что какие-то элементы храняться сверху / сбоку и тд
 public enum EdgePosition {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    FRONT,
-    BACK
+    UP(0),
+    DOWN(1),
+    LEFT(2),
+    RIGHT(3),
+    FRONT(4),
+    BACK(5);
+
+    final int edgeNumber;
+
+    EdgePosition(int edgeNumber) {
+        this.edgeNumber = edgeNumber;
+    }
+
+    public int getEdgeNumber() {
+        return edgeNumber;
+    }
+
 }
