2b750eed245cbd15b64ad83b5311f4bdd0d730f3
TJirden <badert.06.amir@gmail.com>
2026-01-31 23:21:03 +0300

    Смена поворота задней части

--- src/main/java/hse/java/practice/task1/RotationPermutations.java
+++ src/main/java/hse/java/practice/task1/RotationPermutations.java
@@ -54,7 +54,7 @@ public class RotationPermutations {
 
     static final int[][] F_CCW = reverseCycles(F_CW);
 
-    static final int[][] B_CW = {
+    static final int[][] B_СCW = {
             {25, 27, 32, 30},
             {26, 29, 31, 28},
             {19, 33, 6, 48},
@@ -62,7 +62,7 @@ public class RotationPermutations {
             {24, 35, 1, 46}
     };
 
-    static final int[][] B_CCW = reverseCycles(B_CW);
+    static final int[][] B_CW = reverseCycles(B_СCW);
 
     private static int[][] reverseCycles(int[][] cycles) {
         int[][] reversed = new int[cycles.length][];
