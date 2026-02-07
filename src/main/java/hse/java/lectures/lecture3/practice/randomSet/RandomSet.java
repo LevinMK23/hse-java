f591499d57a9ca47e09a5b1e48ced1bf6e00cdd3
Mike <levinmk23@gmail.com>
2026-02-07 19:00:06 +0300

    test

--- src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
+++ src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
@@ -3,6 +3,7 @@ package hse.java.lectures.lecture3.practice.randomSet;
 public class RandomSet<T> {
 
     public boolean insert(T value) {
+
         throw new UnsupportedOperationException("Not implemented");
     }
 
