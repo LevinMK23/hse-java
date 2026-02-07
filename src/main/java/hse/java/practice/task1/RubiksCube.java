615025cc669742282209147381b49b2fb479c276
p1gushka <sani86188@gmail.com>
2026-02-07 20:50:27 +0300

    cube: rerun tests

--- src/main/java/hse/java/practice/task1/RubiksCube.java
+++ src/main/java/hse/java/practice/task1/RubiksCube.java
@@ -2,23 +2,12 @@ package hse.java.practice.task1;
 
 import java.util.Arrays;
 
-/**
- * Необходимо реализовать интерфейс Cube
- * При повороте передней грани, меняются верх низ право и лево
- */
 public class RubiksCube implements Cube {
 
     private static final int EDGES_COUNT = 6;
 
     private final Edge[] edges = new Edge[EDGES_COUNT];
 
-    /**
-     * Создать валидный собранный кубик
-     * грани разместить по ордеру в енуме цветов
-     * грань 0 -> цвет 0
-     * грань 1 -> цвет 1
-     * ...
-     */
     public RubiksCube() {
         CubeColor[] colors = CubeColor.values();
         for (int i = 0; i < 6; i++) {
