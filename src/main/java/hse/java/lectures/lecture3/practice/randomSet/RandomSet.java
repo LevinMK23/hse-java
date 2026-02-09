ef435483a85394f6c0ec65ded3781df6ca1dcd1a
toximu <timofei625211@gmail.com>
2026-02-09 19:43:55 +0300

    randomset: solution

--- src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
+++ src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
@@ -46,8 +46,6 @@ public class RandomSet<T extends Number> {
         } else {
             states[index] = -1;
             size--;
-
-
             Integer index_in_indexes = map_indexes[index];
             indexes[index_in_indexes] = indexes[size];
             map_indexes[indexes[size]] = index_in_indexes;
