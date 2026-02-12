a05133283ed16598166bb180566205769706d8ed
aurhwwh <179162621+aurhwwh@users.noreply.github.com>
2026-02-13 02:56:31 +0300

    randomset: first

--- src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
+++ src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
@@ -1,21 +1,140 @@
 package hse.java.lectures.lecture3.practice.randomSet;
+import java.util.Arrays;
+import java.util.Random;
 
-public class RandomSet<T> {
+import static java.util.Collections.swap;
+
+public class RandomSet<T extends Comparable<T>> {
+
+    static Random rand=new Random();
+    private int root = 0;
+    private int amountOfInts = 0;
+    private int amountOfNodes = 0;
+    private int capacity = 8;
+
+    @SuppressWarnings("unchecked")
+    T[] values = (T[]) new Comparable[8];
+    private static class Node{
+        public int valueIndex;
+        public int left=-1;
+        public int right=-1;
+        public int priority;
+        Node(int valueIndex){
+            this.valueIndex=valueIndex;
+            this.priority = rand.nextInt();
+        }
+    }
+
+    Node[] nodes = new Node[8];
+    int[] numberNodeForValue = new int[8];
+    private void resize(){
+        values = Arrays.copyOf(values,capacity*2);
+        nodes = Arrays.copyOf(nodes,capacity*2);
+        numberNodeForValue = Arrays.copyOf(numberNodeForValue,capacity*2);
+        capacity = capacity * 2;
+    }
+    private int merge(int a, int b){
+        if(a==-1) return b;
+        if(b==-1) return a;
+        if(nodes[a].priority>nodes[b].priority || (nodes[a].priority==nodes[b].priority && values[nodes[a].valueIndex].compareTo(values[nodes[b].valueIndex])>0)){
+            nodes[a].right=merge(nodes[a].right, b);
+            return a;
+        }
+        nodes[b].left=merge(a,nodes[b].left);
+        return b;
+    }
+    class Pair {
+        int a;
+        int b;
+    }
+    private Pair split(int n, T key) {
+        if (n == -1) {
+            Pair p = new Pair();
+            p.a = -1;
+            p.b = -1;
+            return p;
+        }
+        if (values[nodes[n].valueIndex].compareTo(key) < 0) {
+            Pair res = split(nodes[n].right, key);
+            nodes[n].right = res.a;
+            res.a = n;
+            return res;
+        } else {
+            Pair res = split(nodes[n].left, key);
+            nodes[n].left = res.b;
+            res.b = n;
+            return res;
+        }
+    }
+
 
     public boolean insert(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+        if (contains(value)) return false;
+        amountOfInts+=1;
+        if(amountOfNodes>=capacity) resize();
+        values[amountOfInts-1]=value;
+        int newNode = amountOfNodes;
+        nodes[newNode] = new Node(amountOfInts-1);
+        numberNodeForValue[amountOfInts-1]=amountOfNodes;
+        if (amountOfNodes!=0){
+        Pair p = split(root, value);
+        root=merge(merge(p.a,newNode),p.b);
+        }
+        amountOfNodes++;
+        return true;
     }
 
     public boolean remove(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+        if (!contains(value)) return false;
+        Pair p = split(root, value);
+        int current = p.b;
+        int parent = -1;
+        while (nodes[current].left != -1) {
+            parent = current;
+            current = nodes[current].left;
+        }
+        int valueIndexToRemove = nodes[current].valueIndex;
+        int newRightTree;
+        if (parent == -1) {
+            newRightTree = nodes[current].right;
+        } else {
+            nodes[parent].left = nodes[current].right;
+            newRightTree = p.b;
+        }
+        root = merge(p.a, newRightTree);
+
+        int lastIndex = amountOfInts - 1;
+        if (valueIndexToRemove != lastIndex) {
+            T lastValue = values[lastIndex];
+            values[valueIndexToRemove] = lastValue;
+            int movedNode = numberNodeForValue[lastIndex];
+            nodes[movedNode].valueIndex = valueIndexToRemove;
+            numberNodeForValue[valueIndexToRemove] = movedNode;
+        }
+
+        values[lastIndex] = null;
+        amountOfInts--;
+
+        return true;
     }
 
+
     public boolean contains(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+        if(amountOfInts==0) return false;
+        int t = root;
+        while (t!=-1){
+            if(values[nodes[t].valueIndex].equals(value)) return true;
+            else if (values[nodes[t].valueIndex].compareTo(value)<0) {
+                t=nodes[t].right;
+            }
+            else t = nodes[t].left;
+        }
+        return false;
     }
 
     public T getRandom() {
-        throw new UnsupportedOperationException("Not implemented");
+        if(amountOfInts == 0) throw new EmptySetException("");
+        return values[rand.nextInt(amountOfInts)];
     }
 
 }
