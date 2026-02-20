a7f62382059a10813080359cba93e8751b05da10
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-02-20 04:48:48 +0300

    randomset: solution ( add DynamicArray.java)

--- src/main/java/hse/java/lectures/lecture3/practice/randomSet/DynamicArray.java
+++ src/main/java/hse/java/lectures/lecture3/practice/randomSet/DynamicArray.java
@@ -5,7 +5,7 @@ public class DynamicArray<T> {
     private int size;
 
     public DynamicArray() {
-        data = new Object[16];
+        data = new Object[32];
         size = 0 ;
     }
 
@@ -13,29 +13,31 @@ public class DynamicArray<T> {
         return  size;
     }
 
-    public void add(T value) {
-        if ( size == data.length) {
-            Object[] newData = new Object[data.length * 2];
-            System.arraycopy(data , 0 , newData ,0 ,size);
-            data = newData;
-        }
-        data[size] = value;
-        size++;
-    }
     @SuppressWarnings("unchecked")
     public T get(int index) {
-        if ( 0 < index && index < data.length){
-            return (T)data[index] ;
-        }
-        return null;
+        if (index < 0 || index >= size) return null; // Защита
+        return (T) data[index];
     }
-    public void set(int index , T value) {
-        data[index] =value ;
+
+    public void add(T value) {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            System.arraycopy(data, 0, newData, 0, size);
+            data = newData;
+        }
+        data[size++] = value;
+    }
+
+    public void set(int index, T value) {
+        if (index >= 0 && index < size) {
+            data[index] = value;
+        }
     }
 
     @SuppressWarnings("unchecked")
     public T removeLast() {
-        T value = (T) data[size -1] ;
+        if (size == 0) return null;
+        T value = (T) data[size - 1];
         data[size - 1] = null;
         size--;
         return value;
