72c961b53c313623458b3866830721cee8d0c9f4
MuravAna <muravyeva.ab@yandex.ru>
2026-02-28 12:54:50 +0300

    add RandomSet implementation

--- src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
+++ src/main/java/hse/java/lectures/lecture3/practice/randomSet/RandomSet.java
@@ -1,21 +1,141 @@
 package hse.java.lectures.lecture3.practice.randomSet;
 
-public class RandomSet<T> {
+import java.util.Objects;
+import java.util.Random;
 
-    public boolean insert(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+public class RandomSet<T extends Number> {
+    private static class Node<T> {
+        T value;
+        int index;          
+        Node<T> next;
+
+        Node(T value, int index, Node<T> next) {
+            this.value = value;
+            this.index = index;
+            this.next = next;
+        }
     }
 
+    private static final int CAPACITY = 100;
+    private static final int PRIME = 15_485_863;
+
+    private Object[] elements;              
+    private Node<T>[] heads;            
+    private int len;                     
+    private final Random random = new Random();
+
+    @SuppressWarnings("unchecked")
+    public RandomSet() {
+        elements = new Object[CAPACITY];
+        heads = new Node[CAPACITY];
+        len = 0;
+    }
+
+    private int get_hash(T value) {
+        return (value == null ) ? 0 : Math.abs((int) value.longValue() * PRIME) % heads.length;
+    }
+
+    private Node<T> findNode(T value) {
+        int h = get_hash(value);
+        Node<T> cur = heads[h];
+        while (cur != null) {
+            if (Objects.equals(value, cur.value)) {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public boolean insert(T value) {
+        if (findNode(value) != null) {
+            return false;
+        }
+
+        if (len == elements.length) {
+            Object[] newData = new Object[elements.length * 2];
+            for (int i = 0; i < elements.length; i++) {
+                newData[i] = elements[i];
+            }
+            elements = newData;
+        }
+
+        elements[len] = value;
+
+        int h = get_hash(value);
+        heads[h] = new Node<>(value, len, heads[h]);
+
+        len++;
+
+        if (len > heads.length * 0.75) {
+            Node<T>[] oldHeads = heads;
+            heads = new Node[oldHeads.length * 2];
+            for (Node<T> head : oldHeads) {
+                Node<T> cur = head;
+                while (cur != null) {
+                    Node<T> next = cur.next;
+                    int newHash = get_hash(cur.value);
+                    cur.next = heads[newHash];
+                    heads[newHash] = cur;
+                    cur = next;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
     public boolean remove(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+        Node<T> node = findNode(value);
+        if (node == null) {
+            return false;
+        }
+
+        int ind = node.index;
+
+        int h = get_hash(value);
+        Node<T> cur = heads[h];
+        Node<T> prev = null;
+        while (cur != null) {
+            if (cur == node) {
+                if (prev == null) {
+                    heads[h] = cur.next;
+                } else {
+                    prev.next = cur.next;
+                }
+                break;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        
+        if (ind != len - 1) {
+            T lastValue = (T) elements[len - 1];
+            elements[ind] = lastValue;
+
+            Node<T> lastNode = findNode(lastValue);
+            if (lastNode != null) {
+                lastNode.index = ind;
+            }
+        }
+
+        len--;
+
+        return true;
     }
 
     public boolean contains(T value) {
-        throw new UnsupportedOperationException("Not implemented");
+        return findNode(value) != null;
     }
-
+    
+    @SuppressWarnings("unchecked")
     public T getRandom() {
-        throw new UnsupportedOperationException("Not implemented");
+        if (len == 0) {
+            throw new EmptySetException("No elements in the random set");
+        }
+        int randomIndex = random.nextInt(len);
+        return (T) elements[randomIndex];
     }
-
 }
