ec3544acf261579689d6c252fa9e6767761288cf
Alexandr Baranov <sani86188@gmail.com>
2026-03-14 13:18:22 +0300

    add queue task

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,51 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
-public class BoundedBlockingQueue<T> {
+import java.util.LinkedList;
 
+public class BoundedBlockingQueue<T> {
+    private final LinkedList<T> list = new LinkedList<>();
+    private int capacity;
+    private final Object monitor = new Object();
 
     public BoundedBlockingQueue(int capacity) {
-
+        if (capacity <= 0) {
+            throw  new IllegalArgumentException("capacity must be positive");
+        }
+        this.capacity = capacity;
     }
 
-    public void put(T item) {
+    public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new NullPointerException("item must be non-null");
+        }
 
+        synchronized (monitor) {
+            while (list.size() == capacity) {
+                monitor.wait();
+            }
+            list.push(item);
+            notifyAll();
+        }
     }
 
-    public T take() {
-        return null;
+    public T take() throws InterruptedException {
+        synchronized (monitor) {
+            while (list.isEmpty()) {
+                wait();
+            }
+            T item = list.pop();
+            notifyAll();
+            return item;
+        }
     }
 
     public int size() {
-        return 0;
+        synchronized (monitor) {
+            return list.size();
+        }
     }
 
     public int capacity() {
-        return 0;
+        return this.capacity;
     }
 }
