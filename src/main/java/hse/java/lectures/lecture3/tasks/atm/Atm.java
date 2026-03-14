8b5f6460c9a2b523f3cce572ff7e53255eb6ed14
gle6a6y <legostayevg@mail.ru>
2026-03-14 13:50:29 +0300

    queue: fix

--- src/main/java/hse/java/lectures/lecture3/tasks/atm/Atm.java
+++ src/main/java/hse/java/lectures/lecture3/tasks/atm/Atm.java
@@ -16,15 +16,6 @@ public class Atm {
             this.value = value;
         }
 
-        static Denomination fromInt(int v) throws InvalidDepositException{
-            for (Denomination d : values()) {
-                if (d.value == v) {
-                    return d;
-                }
-            }
-            throw new InvalidDepositException("Invalid nominal value");
-        }
-
         int value() {
             return value;
         }
@@ -39,66 +30,16 @@ public class Atm {
     private final Map<Denomination, Integer> banknotes = new EnumMap<>(Denomination.class);
 
     public Atm() {
-        for(Denomination val : banknotes.keySet()) {
-            banknotes.put(val, 0);
-        }
     }
 
-    public void deposit(Map<Integer, Integer> banknotes_) {
-        try {
-            if (banknotes_.isEmpty()) {
-                throw new InvalidDepositException("There is no money");
-            }
-            for (Map.Entry<Integer, Integer> entry : banknotes_.entrySet()) {
-                Denomination val = Denomination.fromInt(entry.getKey());
-                int oldValue = banknotes.get(val);
-                int amountOfNominal = entry.getValue();
-                if (amountOfNominal < 0) {
-                    throw new InvalidDepositException("Negative amount of " + entry.getKey().toString());
-                }
-                int newValue = oldValue + amountOfNominal;
-                banknotes.replace(val, oldValue, newValue);
-            }
-        }
-        catch (InvalidDepositException e) {
-            System.err.println(e.getMessage());
-        }
-    }
+    public void deposit(Map<Denomination, Integer> banknotes){}
 
     public Map<Denomination, Integer> withdraw(int amount) {
-        Map<Denomination, Integer> mapka = new EnumMap<>(Denomination.class);
-        try {
-            if (amount <= 0) {
-                throw new InvalidAmountException("Amount is non positive");
-            }
-            if (amount > getBalance()) {
-                throw new InsufficientFundsException("Amount > atm balance");
-            }
-            for(int val : new int[]{5000, 1000, 500, 100, 50}) {
-                while (amount >= val) {
-                    int oldValue = mapka.get(Denomination.fromInt(val));
-                    mapka.replace(Denomination.fromInt(val), oldValue, oldValue + 1);
-                    amount -= val;
-                }
-            }
-            if (amount != 0) {
-                throw new CannotDispenseException("Can`t dicpense");
-            }
-            return mapka;
-        } catch (InvalidAmountException e) {
-            System.err.println(e.getMessage());
-        } catch (InsufficientFundsException e) {
-            System.err.println(e.getMessage());
-        }
-        return mapka;
+        return Map.of();
     }
 
     public int getBalance() {
-        int sum = 0;
-        for(Map.Entry<Denomination, Integer> entry : banknotes.entrySet()) {
-            sum += entry.getKey().value() * entry.getValue();
-        }
-        return sum;
+        return 0;
     }
 
 }
