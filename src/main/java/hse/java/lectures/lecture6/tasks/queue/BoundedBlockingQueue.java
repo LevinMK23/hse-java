cfaaa596a040197d45ec2d838cf97e4999f60ddd
gle6a6y <legostayevg@mail.ru>
2026-03-14 13:18:48 +0300

    queue: first try

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,59 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
+import java.util.LinkedList;
+
 public class BoundedBlockingQueue<T> {
 
+    final Object monitor;
+    int capacity;
+    LinkedList<T> queue;
+    int curr_len;
 
-    public BoundedBlockingQueue(int capacity) {
+    public BoundedBlockingQueue(int capacity_) throws IllegalArgumentException {
 
+        if (capacity_ <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        monitor = new Object();
+        capacity = capacity_;
+        queue = new LinkedList<>();
+        curr_len = 0;
     }
 
-    public void put(T item) {
-
+    public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        synchronized (monitor) {
+            while (curr_len >= capacity) {
+                monitor.wait();
+            }
+            queue.offer(item);
+            curr_len++;
+            monitor.notifyAll();
+        }
     }
 
-    public T take() {
-        return null;
+    public T take() throws InterruptedException {
+        synchronized (monitor) {
+            while (curr_len <= 0) {
+                monitor.wait();
+            }
+            curr_len--;
+            T item = queue.poll();
+            monitor.notifyAll();
+            return item;
+        }
     }
 
     public int size() {
-        return 0;
+        synchronized (monitor) {
+            return curr_len;
+        }
     }
 
     public int capacity() {
-        return 0;
+        return capacity;
     }
 }
