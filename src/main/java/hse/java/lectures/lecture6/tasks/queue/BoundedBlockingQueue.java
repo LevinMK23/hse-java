2c2d110aed08511373714377019639169ec71d30
ParovozikThomas <toni.podrezov@gmail.com>
2026-03-14 13:47:15 +0300

    queue: 3-rd attempt

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -29,7 +29,7 @@ public class BoundedBlockingQueue<T> {
             while (fill == capacity) {
                 this.wait();
             }
-
+            array[tail] = item;
             tail = (tail + 1) % capacity;
             fill++;
             this.notifyAll();
