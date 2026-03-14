f3a34c84cf5e1b8c05139e12b377f7555af74e04
ParovozikThomas <toni.podrezov@gmail.com>
2026-03-14 13:41:42 +0300

    queue: first attempt

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -2,24 +2,67 @@ package hse.java.lectures.lecture6.tasks.queue;
 
 public class BoundedBlockingQueue<T> {
 
+    private final Object[] array;
+    int capacity;
+    int tail;
+    int head;
+    int fill;
+
 
     public BoundedBlockingQueue(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.array = new Object[capacity];
+        this.capacity = capacity;
+
+        this.head = capacity - 1;
+        this.tail = this.head;
+        this.fill = 0;
+    }
+
+    public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        synchronized (this) {
+            while (fill == capacity) {
+                this.wait();
+            }
+
+            tail = (tail + 1) % capacity;
+            fill++;
+            this.notifyAll();
+
+        }
 
     }
 
-    public void put(T item) {
+    public T take() throws InterruptedException {
+        synchronized (this) {
+            while (fill == 0) {
+                wait();
+            }
 
-    }
+            T item = (T) array[head];
+            array[head] = null;
+            head = (head + 1) % capacity;
+            fill--;
 
-    public T take() {
-        return null;
+            this.notifyAll();
+            return item;
+
+        }
+
+
     }
 
     public int size() {
-        return 0;
+        return fill;
     }
 
     public int capacity() {
-        return 0;
+
+        return capacity;
     }
 }
